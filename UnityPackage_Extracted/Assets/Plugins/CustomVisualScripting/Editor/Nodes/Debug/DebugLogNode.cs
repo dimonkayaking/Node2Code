64e6517a5c218e179a9fa4c2b9fe68104ab48b51
Дмитрий <dmmurzaev@mail.ru>
2026-03-28 21:37:56 +0500

    Update DebugLogNode.cs

--- UnityPackage_Extracted/Assets/Plugins/CustomVisualScripting/Editor/Nodes/Debug/DebugLogNode.cs
+++ UnityPackage_Extracted/Assets/Plugins/CustomVisualScripting/Editor/Nodes/Debug/DebugLogNode.cs
@@ -1,1 +1,19 @@
+using System;
+using UnityEngine;
+using GraphProcessor;
+using VisualScripting.Core.Models;
+using CustomVisualScripting.Editor.Nodes.Base;
 
+namespace CustomVisualScripting.Editor.Nodes.Debug
+{
+    [System.Serializable, NodeMenuItem("Debug/Debug Log")]
+    public class DebugLogNode : BaseExecutionNode
+    {
+        public override NodeType NodeType => NodeType.DebugLog;
+
+        [Input("message")]
+        public object message;
+
+        public override string name => "Debug Log";
+    }
+}
